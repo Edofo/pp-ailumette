11b1cf010195df34a7495b749476eb7bc207b1c0
Edofo <Edofo@users.noreply.github.com>
2021-01-22 15:43:01 +0100

    Add IA

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 const readline = require('readline');
 
+const map = require('./map');
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-  });
+});
 
 
+// Initialisé le nombre d'allumettes
+let nbrAllumettes = 16;
 
-const map = []
 
-const columns = 9
-const rows = 6
+async function Afficher() {
+    
+    display(map);
+    
+    //Dire à la personne de choisir ou supprimer et le nombre 1 à 3 allumettes
+    try {
+        return await Player();
+    } catch(e) {
+        console.error("Un problème est survenue !")
+    };
+    
+    //IA qui supprime
+    IA();
 
-for (let x = 0; x < rows; x++) {
-    for (let y = 0; y < columns; y++) {
-        if (!map[x]) {
-            map[x] = []
-        }
-        
-        let element = ' '
-        if (x === 0 || y === 0 || y === columns -1 || x === rows -1) {
-            element = '*'
-        }
+};
 
-        map[x][y] = element
-    }
-}
+Afficher()
+
 
 function display(values) {
     const rows = values.length
@@ -35,80 +39,92 @@ function display(values) {
     }
 }
 
-display(map);
+function sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }   
 
+function Player() {
 
-// Initialisé le nombre d'allumettes
-let nbrAllumettes = 16;
-
-
-function Afficher() {
+    console.log('A votre tour !')
+    rl.question('Choisis une ligne : ', (valueOne) => {
+        nbrLignes = parseInt(valueOne);
     
-    /* BOUCLE */
-    if (!nbrAllumettes == 0) {
-        //Dire à la personne de choisir ou supprimer et le nombre 1 à 3 allumettes
-        function chooseALine() {
-
-            rl.question('Choisis une ligne : ', (valueOne) => {
-                nbrLignes = parseInt(valueOne);
-          
-                if (nbrLignes <= 4 && nbrLignes >= 1) {
-                    console.log(`Ligne ${nbrLignes} confirmée`);
+        if (nbrLignes <= 4 && nbrLignes >= 1) {
+            console.log(`Ligne ${nbrLignes} confirmée`);
+    
+            rl.question('Choisis un chiffre : ', (valueTwo) => {
+            nbrChiffre = parseInt(valueTwo);
             
-                    rl.question('Choisis un chiffre : ', (valueTwo) => {
-                    nbrChiffre = parseInt(valueTwo);
+            if (nbrChiffre <= 3 && nbrChiffre >= 1) {
+    
+                if (nbrChiffre <= nbrAllumettes) {
+    
+                    console.log(`Chiffre ${nbrChiffre} confirmé`);
                     
-                    if (nbrChiffre <= 3 && nbrChiffre >= 1) {
-            
-                        if (nbrChiffre <= nbrAllumettes) {
-            
-                            console.log(`Chiffre ${nbrChiffre} confirmé`);
-                            
-                            nbrAllumettes = nbrAllumettes - nbrChiffre
+                    nbrAllumettes = nbrAllumettes - nbrChiffre
 
-                            if (nbrAllumettes == 0) {
+                    if (nbrAllumettes == 0) {
 
-                                    console.log("Vous avez perdu");
-                                    display(map);
-                                    rl.close();
-                            } else {
-                                    console.log(`Il reste ${nbrAllumettes} allumettes`);
-                                    display(map);
-                            }
-            
-                        } else {
-                            console.error('Erreur : Il n\'y a pas assez d\'allumettes');
-                            chooseALine();
-                            return;  
-                        }
-            
+                        console.log("Vous avez perdu");
+                        display(map);
+                        rl.close();
+                        return; 
+
                     } else {
-                        console.error('Erreur : Vous devez choisir un chiffre entre 1 et 3');
-                        chooseALine();
-                        return;  
+
+                        console.log(`Il reste ${nbrAllumettes} allumettes`);
+                        display(map);
+                        IA()
+                        return; 
                     }
-            
-                    });
-            
+    
                 } else {
-                    console.error('Erreur : Vous devez choisir une ligne entre 1 et 4');
-                    chooseALine();
-                    return;
+                    console.error('Erreur : Il n\'y a pas assez d\'allumettes');
+                    Player();
+                    return;  
                 }
+    
+            } else {
+                console.error('Erreur : Vous devez choisir un chiffre entre 1 et 3');
+                Player();
+                return;  
+            }
+    
             });
+    
+        } else {
+            console.error('Erreur : Vous devez choisir une ligne entre 1 et 4');
+            Player();
+            return;
         }
-          
-        chooseALine();          
-        //IA qui supprime
+    });
+};
 
-        //Raffiché le tableau
+async function IA() {
 
+    const nbrChiffre = Math.floor(Math.random() * 3) +1 ;
+
+    nbrAllumettes = nbrAllumettes - nbrChiffre
+
+    console.log("Au tour de l'IA")
+
+    await sleep(3000);
+
+    if (nbrAllumettes == 0) {
+
+        console.log("Vous avez gagné !");
+        display(map);
+        rl.close();
+        return; 
+        
     } else {
+        
+        console.log(`L'IA a retiré ${nbrChiffre} allumettes`);
+        console.log(`Il reste ${nbrAllumettes} allumettes`);
+        display(map);
         Afficher()
+        return; 
     }
-    /* BOUCLE */
-}
-
-Afficher()
-
-
+};
--- player.js
+++ player.js
@@ -5,55 +5,61 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-let nbrAllumettes = 16
+function Player() {
 
-function chooseALine() {
-
-    rl.question('Choisis une ligne : ', (valueOne) => {
+  console.log('A votre tour !')
+  rl.question('Choisis une ligne : ', (valueOne) => {
       nbrLignes = parseInt(valueOne);
   
       if (nbrLignes <= 4 && nbrLignes >= 1) {
-        console.log(`Ligne ${nbrLignes} confirmée`);
+          console.log(`Ligne ${nbrLignes} confirmée`);
   
-        rl.question('Choisis un chiffre : ', (valueTwo) => {
+          rl.question('Choisis un chiffre : ', (valueTwo) => {
           nbrChiffre = parseInt(valueTwo);
           
           if (nbrChiffre <= 3 && nbrChiffre >= 1) {
   
               if (nbrChiffre <= nbrAllumettes) {
   
-                  console.log(`Chiffre ${nbrChiffre} confirmée`);
+                  console.log(`Chiffre ${nbrChiffre} confirmé`);
                   
                   nbrAllumettes = nbrAllumettes - nbrChiffre
 
                   if (nbrAllumettes == 0) {
 
-                        console.log("Vous avez perdu");
-                        rl.close();
+                      console.log("Vous avez perdu");
+                      display(map);
+                      rl.close();
+                      return; 
+
                   } else {
-                        console.log(`Il reste ${nbrAllumettes} allumettes`);
+
+                      console.log(`Il reste ${nbrAllumettes} allumettes`);
+                      display(map);
+                      IA()
+                      return; 
                   }
   
               } else {
                   console.error('Erreur : Il n\'y a pas assez d\'allumettes');
-                  chooseALine();
+                  Player();
                   return;  
               }
   
           } else {
               console.error('Erreur : Vous devez choisir un chiffre entre 1 et 3');
-              chooseALine();
+              Player();
               return;  
           }
   
-        });
+          });
   
       } else {
-        console.error('Erreur : Vous devez choisir une ligne entre 1 et 4');
-        chooseALine();
-        return;
+          console.error('Erreur : Vous devez choisir une ligne entre 1 et 4');
+          Player();
+          return;
       }
-    });
-}
-  
-chooseALine(); 
+  });
+};
+
+Player();
